377eceb7acc6d6ef78c9cce629bdbca1973bf541
Kevin-Perdomo <kevin.siqueira23@gmail.com>
2024-03-11 00:18:16 -0300

    learning

---
+++ Teste_01/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("\n=========================================\n\n");
+    println!("Hello, world!\n");
+    let name = "Kevin Siqueira Perdomo";
+    println!("{} here", name);
+    println!("\n\n=========================================\n");
+}
+
